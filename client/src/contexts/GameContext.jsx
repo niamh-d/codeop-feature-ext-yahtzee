2ad836e0561f87f36b34ec8b005cb11dc8530956
Niamh <niamh.linguist@gmail.com>
2024-03-12 22:20:06 +0200

    basic upper scoring logic working

--- client/src/contexts/GameContext.jsx
+++ client/src/contexts/GameContext.jsx
@@ -1,13 +1,21 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-refresh/only-export-components */
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 
 const GameContext = createContext();
 
 const initialState = {
   rolledDice: [],
   heldDice: [],
+  scoringCells: {
+    aces: null,
+    twos: null,
+    threes: null,
+    fours: null,
+    fives: null,
+    sixes: null,
+  },
 };
 
 const randInt = () => Math.floor(Math.random() * 6) + 1;
@@ -18,6 +26,8 @@ function reducer(state, action) {
       return { ...state, rolledDice: action.payload };
     case "SET_HELD_DICE":
       return { ...state, heldDice: action.payload };
+    case "SET_SCORING_CELLS":
+      return { ...state, scoringCells: action.payload };
     default:
       throw new Error("Unknown action type");
   }
@@ -28,6 +38,35 @@ function GameProvider({ children }) {
     reducer,
     initialState
   );
+
+  useEffect(() => {
+    calculateQualifyingScoringCell(rolledDice);
+  }, [rolledDice]);
+
+  function calculateQualifyingScoringCell(rolledDice) {
+    const filterDiceAndCalculateLength = (dice, val) => {
+      return dice.filter((num) => num === val).length;
+    };
+
+    const scores = {};
+
+    const conditionNames = [
+      "aces",
+      "twos",
+      "threes",
+      "fours",
+      "fives",
+      "sixes",
+    ];
+
+    conditionNames.forEach((condition, i) => {
+      const length = filterDiceAndCalculateLength(rolledDice, i + 1);
+
+      if (length) scores[`${condition}`] = length * (i + 1);
+    });
+
+    dispatch({ type: "SET_SCORING_CELLS", payload: scores });
+  }
 
   function rollDice() {
     const diceArr = [...Array(5)].map((_) => randInt());
