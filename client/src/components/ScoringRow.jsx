4a34f47c1240924c7014039d4035ef67bd6e8ec6
Niamh <niamh.linguist@gmail.com>
2024-03-15 13:42:36 +0200

    prettified condition names in scorecard

--- client/src/components/ScoringRow.jsx
+++ client/src/components/ScoringRow.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
 const ScoringRow = ({ criterionName, score, handler }) => {
+  const criterionTransformString = (criterionName) => {
+    switch (criterionName) {
+      case "threeKind":
+        return "Three of a Kind";
+      case "fourKind":
+        return "Four of a Kind";
+      case "fullHouse":
+        return "Full House";
+      case "smallStraight":
+        return "Small Straight";
+      case "largeStraight":
+        return "Large Straight";
+      case "yahtzee":
+        return "Yahtzee";
+      case "chance":
+        return "Chance";
+      default:
+        return criterionName[0].toUpperCase() + criterionName.slice(1);
+    }
+  };
+
   return (
     <tr>
-      <td>{criterionName}</td>
+      <td>{criterionTransformString(criterionName)}</td>
       <td onClick={() => handler(criterionName, score)}>
         {score ? score : ""}
       </td>
