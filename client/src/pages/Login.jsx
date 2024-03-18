61b6c1e5d4a3e4628cb378948ca42964fe1e5815
Niamh <niamh.linguist@gmail.com>
2024-03-18 21:32:57 +0200

    styled login and set up

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import styles from "./Login.module.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <main>
+      <section className={styles.login}>
+        <div className={styles["login-heading-box"]}>
+          <h2 className={styles["login-heading"]}>Login</h2>
+          <button className="btn btn-primary">Login</button>
+        </div>
+        <form>
+          <label htmlFor="Email">Email</label>
+          <input type="email" />
+          <label htmlFor="Password">Password</label>
+          <input type="password" />
+        </form>
+      </section>
+    </main>
+  );
 };
 
 export default Login;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Signup.module.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <main>
+      <section className={styles.signup}>
+        <div className={styles["signup-heading-box"]}>
+          <h2 className={styles["signup-heading"]}>Sign up</h2>
+          <button className="btn btn-primary">Sign up</button>
+        </div>
+        <form>
+          <label htmlFor="First name">First name</label>
+          <input type="text" />
+          <label htmlFor="Last name">Last name</label>
+          <input type="text" />
+          <label htmlFor="Email">Email</label>
+          <input type="email" />
+          <label htmlFor="Password">Password</label>
+          <input type="password" />
+        </form>
+      </section>
+    </main>
+  );
 };
 
 export default Signup;
