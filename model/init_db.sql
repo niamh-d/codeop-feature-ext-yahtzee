bd7cffeb8f778e604889f27dd1e02218583b648f
Niamh <niamh.linguist@gmail.com>
2024-03-19 18:28:46 +0200

    set up starter sql with 4 users with hashed pws

--- model/init_db.sql
+++ model/init_db.sql
@@ -28,7 +28,10 @@ CREATE TABLE plays(
 
 INSERT INTO users(firstname, lastname, username, email, password)
 VALUES
-    ("Julie", "Andrews", "julie001", "julie@email.com", "qwerty");
+    ("Julie", "Andrews", "julezzz", "julezzz@email.com", "$2b$12$S2Ue31h4S9.EGZQaG2tQue3kpQsAZmTk67HyRLF3k4tSXcJs30CvG"),
+    ("Peter", "Piper", "pp100", "pp100@email.com", "$2b$12$upMHfgUFpVuiqK2RLtBICOjIsmoQGA7eU19qHDUJ/ZhUKVilj5UW6"),
+    ("Patrick", "Fitz", "fitzy100", "fitzy100@email.com", "$2b$12$L96.BCu9XTy4f/8nkxsmnOg04v8QNwHDuCX9EFSKuo8UG1mz84qs6"),
+    ("Sophie", "Shore", "shorezy11", "shorezy11@email.com", "$2b$12$FuQTuzNlzsBcZYtSGbhvU.dlwz7nPi3H9mw1fJINjYF2t8SDVMVBq");
 
 
 INSERT INTO plays(date_played, user_id, game_number, total_score_game, yatzee_score_count, total_upper_wo_bonus, total_lower_wo_bonus, rounds_played, full_game)
