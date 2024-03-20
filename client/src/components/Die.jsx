b5a988ebe95a2a1d67c9f7aee54e457bf4e6ce5e
Niamh <niamh.linguist@gmail.com>
2024-03-20 21:14:04 +0200

    dice added

--- client/src/components/Die.jsx
+++ client/src/components/Die.jsx
@@ -1,11 +1,27 @@
 import styles from "./Die.module.css";
 
+const diceStylesStrs = {
+  1: { transform: "rotateX(0deg) rotateY(0deg)" },
+  2: { transform: "rotateX(0deg) rotateY(0deg)" },
+  3: { transform: "rotateX(0deg) rotateY(90deg)" },
+  4: { transform: "rotateX(0deg) rotateY(-90deg)" },
+  5: { transform: "rotateX(90deg) rotateY(0deg)" },
+  6: { transform: "rotateX(180deg) rotateY(0deg)" },
+};
+
 const Die = ({ num, id, handler }) => {
   return (
-    <div className={styles.die}>
-      <span className={styles.die__number} onClick={() => handler(id)}>
-        {num}
-      </span>
+    <div
+      className={styles.die}
+      style={diceStylesStrs[num]}
+      onClick={() => handler(id)}
+    >
+      <div className={`${styles.face} ${styles.front}`}></div>
+      <div className={`${styles.face} ${styles.back}`}></div>
+      <div className={`${styles.face} ${styles.top}`}></div>
+      <div className={`${styles.face} ${styles.bottom}`}></div>
+      <div className={`${styles.face} ${styles.right}`}></div>
+      <div className={`${styles.face} ${styles.left}`}></div>
     </div>
   );
 };
