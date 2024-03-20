a1255fa03ea5dc71de75ec2bb01e42f04b7fb7e3
Niamh <niamh.linguist@gmail.com>
2024-03-20 17:40:49 +0200

    fixed typo in plays route file

--- routes/plays.js
+++ routes/plays.js
@@ -33,7 +33,7 @@ router.post("/", async function (req, res, next) {
 
     const timestampStr = `${date} ${timeClean}`;
 
-    await db(`INSERT INTO plays(date_played, user_id, game_number, total_score_game, yatzee_score_count, total_upper_wo_bonus, total_lower_wo_bonus, rounds_played, full_game)
+    await db(`INSERT INTO plays(date_played, user_id, game_number, total_score_game, yahtzee_score_count, total_upper_wo_bonus, total_lower_wo_bonus, rounds_played, full_game)
     VALUES
         ('${timestampStr}', ${userId}, ${countGame}, ${grandTotalGameScored}, ${yahtzeeScoreCount}, ${upperTotalScored}, ${lowerTotalScored}, ${countRound}, ${gameComplete});`);
 
