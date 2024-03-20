7b7f590b1b6038badd8c19772ebd58891f13b4bc
Niamh <niamh.linguist@gmail.com>
2024-03-20 15:08:40 +0200

    conditionally rendering of play plays table

--- client/src/components/PastPlaysTable.jsx
+++ client/src/components/PastPlaysTable.jsx
@@ -6,26 +6,31 @@ const PastPlaysTable = () => {
   const { pastPlays } = useSession();
 
   return (
-    <table className="table-plays">
-      <thead>
-        <tr>
-          <th>Date Played</th>
-          <th>Game Number</th>
-          <th>Total Score</th>
-          <th>Upper Total</th>
-          <th>Upper Bonus Scored</th>
-          <th>Number of Yahtzees</th>
-          <th>Lower Total</th>
-          <th>Rounds Played</th>
-          <th>Complete Game</th>
-        </tr>
-      </thead>
-      <tbody>
-        {pastPlays.map((play) => (
-          <PastPlaysRow play={play} key={play.id} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      {pastPlays.length > 0 && (
+        <table className="table-plays">
+          <thead>
+            <tr>
+              <th>Date Played</th>
+              <th>Game Number</th>
+              <th>Total Score</th>
+              <th>Upper Total</th>
+              <th>Upper Bonus Scored</th>
+              <th>Number of Yahtzees</th>
+              <th>Lower Total</th>
+              <th>Rounds Played</th>
+              <th>Complete Game</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pastPlays.map((play) => (
+              <PastPlaysRow play={play} key={play.id} />
+            ))}
+          </tbody>
+        </table>
+      )}
+      {pastPlays.length === 0 && <p>You do not have any past plays saved.</p>}
+    </>
   );
 };
 
